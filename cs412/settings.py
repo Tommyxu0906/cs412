cb37969b9bd40c723399a9fc1a5580f043bb7bc9
Kanghuan Xu <tommyxu@bu.edu>
2024-09-28 22:44:10 -0400

    debugging static

--- cs412/settings.py
+++ cs412/settings.py
@@ -118,7 +118,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
