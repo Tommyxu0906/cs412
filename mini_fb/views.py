2b9ab149d20d01e83f247afdd7fa52ec08d15daf
Kanghuan Xu <tommyxu@bu.edu>
2024-10-03 15:10:54 -0400

    Added views and template for mini_fb

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,11 @@
+#mini_fb/views.py
+#import modules for the views
 from django.shortcuts import render
-
+from .models import Profile
+from django.views.generic import ListView
 # Create your views here.
+
+class ShowAllProfilesView(ListView):
+    model = Profile
+    template_name = 'mini_fb/show_all_profiles.html'
+    context_object_name = 'profiles'
