055fac2f09f03a347b948ca4e87fb09804e0b043
Kanghuan Xu <tommyxu@bu.edu>
2024-10-05 11:20:03 -0400

    changed name to a5_url.txt

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 # Create your views here.
 
 class ShowAllProfilesView(ListView):
+    """creates view for minifb"""
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
