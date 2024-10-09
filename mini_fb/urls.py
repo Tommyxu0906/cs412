bb0c0701eb4b2edce45083142f8f36e19c819cff
Kanghuan Xu <tommyxu@bu.edu>
2024-10-08 21:34:51 -0400

    Task1 completed successfully

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,7 +1,8 @@
 #mini_fb/urls.py imports modules
 from django.urls import path
-from .views import ShowAllProfilesView
+from .views import ShowAllProfilesView, ShowProfilePageView
 
 urlpatterns = [
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile_page'),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from .models import Profile
 from django.views.generic import ListView
+from django.views.generic import DetailView
 # Create your views here.
 
 class ShowAllProfilesView(ListView):
@@ -10,3 +11,9 @@ class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class ShowProfilePageView(DetailView):
+    '''creates view for individual profile'''
+    model = Profile
+    template_name = 'mini_fb/show_profile_page.html'
+    context_object_name = 'profile'
