ef2274b8502081d3905ee3a0a41c70d02e3a4850
Kanghuan Xu <tommyxu@bu.edu>
2024-10-09 10:12:17 -0400

    statusmessage added

--- mini_fb/admin.py
+++ mini_fb/admin.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+'''registers models to admin'''
 admin.site.register(Profile)
+admin.site.register(StatusMessage)
 
--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,10 +1,12 @@
 # mini_fb/models.py
 # define the data objects in mini_fb project
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
+
 class Profile(models.Model):
+    '''profile class defines attributes of each profile'''
     first_name = models.TextField(blank=False)
     last_name = models.TextField(blank=False)
     city = models.TextField(blank=False)
@@ -13,3 +15,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+    
+    def get_status_messages(self):
+        return StatusMessage.objects.filter(profile=self).order_by('-timestamp')
+
+class StatusMessage(models.Model):
+    '''status message class for mini_fb'''
+    timestamp = models.DateTimeField(default=timezone.now)
+    message = models.TextField(blank=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='status_messages')
+
+    def __str__(self):
+        return f"{self.profile.first_name} {self.profile.last_name}: {self.message[:20]}..."
