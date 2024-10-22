a81aabdfb7f278934e287ec9d7651b0984087992
Kanghuan Xu <tommyxu@bu.edu>
2024-10-22 10:26:11 -0400

    Task1 completed

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -21,6 +21,13 @@ class Profile(models.Model):
     
     def get_absolute_url(self):
         return reverse('show_profile', kwargs={'pk': self.pk})
+    
+    #gets friends for each profile
+    def get_friends(self):
+        friends_as_profile1 = Friend.objects.filter(profile1=self).values_list('profile2', flat=True)
+        friends_as_profile2 = Friend.objects.filter(profile2=self).values_list('profile1', flat=True)
+        friend_ids = list(friends_as_profile1) + list(friends_as_profile2)
+        return Profile.objects.filter(id__in=friend_ids)
 
 class StatusMessage(models.Model):
     '''status message class for mini_fb'''
@@ -43,3 +50,11 @@ class Image(models.Model):
 
     def __str__(self):
         return f"Image for {self.status_message}"
+
+class Friend(models.Model):
+    profile1 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='friends1')
+    profile2 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='friends2')
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.profile1.first_name} {self.profile1.last_name} & {self.profile2.first_name} {self.profile2.last_name}'
