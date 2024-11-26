3b837666d50b8d76e59aecc736e97689d1d8222d
Kanghuan Xu <tommyxu@bu.edu>
2024-11-26 10:27:06 -0500

    Fixed static files configuration

--- cs412/settings.py
+++ cs412/settings.py
@@ -117,14 +117,12 @@ USE_TZ = True
 
 import os
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
