4a04a62d231e2e9dbc361a41d7fa10715608718c
Kanghuan Xu <tommyxu@bu.edu>
2024-11-26 10:23:31 -0500

    try to solve static

--- project/urls.py
+++ project/urls.py
@@ -30,3 +30,6 @@ urlpatterns = [
     path('checkout/cart/', CartCheckoutView.as_view(), name='checkout_cart'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
